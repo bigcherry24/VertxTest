1eecac982cd948475c85b99a601b7177e606e161
bigcherry24 <cherry24mint@naver.com>
2024-12-30 14:11:28 +0000

    modify error

--- vertx-http-server/src/main/java/com/example/OrderClientVerticle.java
+++ vertx-http-server/src/main/java/com/example/OrderClientVerticle.java
@@ -3,6 +3,7 @@ package com.example;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpMethod;
 
 public class OrderClientVerticle extends AbstractVerticle {
 
@@ -10,12 +11,20 @@ public class OrderClientVerticle extends AbstractVerticle {
   public void start() {
     HttpClient client = vertx.createHttpClient();
 
-    client.getNow(8080, "localhost", "/order?id=1", response -> {
-      response.bodyHandler(body -> {
-        System.out.println("Received response with status code " + response.statusCode());
-        System.out.println("Response body: " + body.toString());
+    // HTTP GET 요청 생성 및 전송
+    client.request(HttpMethod.GET, 8080, "localhost", "/order?id=1")
+      .onSuccess(request -> {
+        request.response().onSuccess(response -> {
+          response.bodyHandler(body -> {
+            System.out.println("Received response with status code " + response.statusCode());
+            System.out.println("Response body: " + body.toString());
+          });
+        });
+        request.end();
+      })
+      .onFailure(err -> {
+        System.err.println("Failed to send request: " + err.getMessage());
       });
-    });
   }
 
   public static void main(String[] args) {
--- vertx-http-server/src/main/java/com/example/OrderServiceVerticle.java
+++ vertx-http-server/src/main/java/com/example/OrderServiceVerticle.java
@@ -2,10 +2,14 @@ package com.example;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLClient;
 import io.vertx.ext.sql.SQLConnection;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
 
 public class OrderServiceVerticle extends AbstractVerticle {
 
